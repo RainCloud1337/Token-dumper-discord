d3e45199d6fd4719b545a2e73123a953957f30a4
raincloudskid <72419668+raincloudskid@users.noreply.github.com>
2020-10-05 16:12:44 -0700

    Create realdump.py

---
+++ realdump.py
@@ -0,0 +1,18 @@
+import colorama
+from colorama import Fore
+
+skid1 = """
+i'm a skid, my name is raincloud and I stole this from HellSec.
+"""
+
+skid2 = """
+i'm a skid, my name is raincloud and I stole this from HellSec.
+"""
+
+skid3 = """
+i'm a skid, my name is raincloud and I stole this from HellSec.
+"""
+
+print(Fore.RED+skid1)
+print(Fore.RED+skid2)
+print(Fore.RED+skid3)
